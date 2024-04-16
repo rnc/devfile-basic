61d68ebfb18ff63483a9ab11a73cc1538785a9e5
Nick Cross <ncross@redhat.com>
2024-04-16 16:09:24 +0100

    Use diagnostic dockerfile

--- build.sh
+++ build.sh
@@ -61,6 +61,52 @@ mkdir -p /root/project/logs /root/project/packages /root/project/build-info
 
 #This is replaced when the task is created by the golang code
 
+#!/usr/bin/env bash
+
+cat >"/root/software/settings"/settings.xml <<EOF
+<settings>
+  <mirrors>
+    <mirror>
+      <id>mirror.default</id>
+      <url>${CACHE_URL}</url>
+      <mirrorOf>*</mirrorOf>
+    </mirror>
+  </mirrors>
+
+  <!-- Off by default, but allows a secondary Maven build to use results of prior (e.g. Gradle) deployment -->
+  <profiles>
+    <profile>
+      <id>gradle</id>
+      <activation>
+        <property>
+          <name>useJBSDeployed</name>
+        </property>
+      </activation>
+      <repositories>
+        <repository>
+          <id>artifacts</id>
+          <url>file:///root/project/artifacts</url>
+          <releases>
+            <enabled>true</enabled>
+            <checksumPolicy>ignore</checksumPolicy>
+          </releases>
+        </repository>
+      </repositories>
+      <pluginRepositories>
+        <pluginRepository>
+          <id>artifacts</id>
+          <url>file:///root/project/artifacts</url>
+          <releases>
+            <enabled>true</enabled>
+            <checksumPolicy>ignore</checksumPolicy>
+          </releases>
+        </pluginRepository>
+      </pluginRepositories>
+    </profile>
+  </profiles>
+</settings>
+EOF
+
 
 #!/usr/bin/env bash
 export GRADLE_USER_HOME="/root/software/settings/.gradle"
--- preprocessor.sh
+++ preprocessor.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# Gradle
 /root/software/system-java/bin/java -jar /root/software/build-request-processor/quarkus-run.jar gradle-prepare /root/project/workspace -dp com.diffplug.gradle.spotless -dp kotlin.gradle.targets.js -dp org.gradle.api.plugins.quality -dp org.gradle.plugins.signing -dp org.jetbrains.dokka
+
+# Maven
+# /root/software/system-java/bin/java -jar /root/software/build-request-processor/quarkus-run.jar maven-prepare /root/project/workspace -dp org.glassfish.copyright:glassfish-copyright-maven-plugin -dp org.sonatype.plugins:nexus-staging-maven-plugin -dp com.mycila:license-maven-plugin -dp org.codehaus.mojo:findbugs-maven-plugin -dp de.jjohannes:gradle-module-metadata-maven-plugin -dp org.codehaus.mojo:cobertura-maven-plugin -dp com.diffplug.spotless:spotless-maven-plugin -dp io.fabric8:docker-maven-plugin -dp org.apache.rat:apache-rat-plugin
